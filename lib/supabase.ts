eb4fb6db518aaeb291c73ccfda8a2230d8af4d20
Sathvik Kandadi <sathvikkandadi@gmail.com>
2025-04-22 10:56:32 +0530

    Changes

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your Supabase project URL and anon key
-const supabaseUrl = 'https://milwsihjhjxnpioqncjl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pbHdzaWhqaGp4bnBpb3FuY2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3NjEyODAsImV4cCI6MjA1OTMzNzI4MH0.d7bVTQnyg8OEQ4cNJXCj7mH-BbBx-Zgm-etw2xIEyGo';
+const supabaseUrl = 'You Supabase URL';
+const supabaseAnonKey = 'Your Supabase AnonKey';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
